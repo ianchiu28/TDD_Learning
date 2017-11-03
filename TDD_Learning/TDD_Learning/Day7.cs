7116b073b2b9c6ea9de4d34f84a0e6515aa14e77
ianchiu28 <nk9710632@gmail.com>
2017-11-03 11:52:19 +0800

    Day7 Update!

---
+++ TDD_Learning/TDD_Learning/Day7.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Learning
+{
+    class Day7
+    {
+    }
+
+    public interface ICheckInFee
+    {
+        decimal GetFee(Customer customer);
+    }
+
+    public class Customer
+    {
+        public bool IsMale { get; set; }
+
+        public int Seq { get; set; }
+    }
+
+    public class Pub
+    {
+        private ICheckInFee _checkInFee;
+        private decimal _inCome = 0;
+
+        public Pub(ICheckInFee checkInFee)
+        {
+            this._checkInFee = checkInFee;
+        }
+
+        public int CheckIn(List<Customer> customers)
+        {
+            var result = 0;
+
+            foreach(var customer in customers)
+            {
+                var isFemale = !customer.IsMale;
+
+                // only men need to pay
+                if (isFemale)
+                {
+                    continue;
+                }
+                else
+                {
+                    // for stub, validate status: income value
+                    // for mock, validate only male
+                    this._inCome += this._checkInFee.GetFee(customer);
+
+                    result++;
+                }
+            }
+
+            // for stub, validate return value
+            return result;
+        }
+
+        public decimal GetInCome()
+        {
+            return this._inCome;
+        }
+    }
+}
---
+++ TDD_Learning/TDD_LearningTests/PubTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Learning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Mocks;
+
+namespace TDD_Learning.Tests
+{
+    [TestClass()]
+    public class PubTests
+    {
+        [TestMethod()]
+        public void Test_Charge_Customer_Count()
+        {
+            // arrange
+            ICheckInFee stubCheckInFee = MockRepository.GenerateStub<ICheckInFee>();
+            Pub target = new Pub(stubCheckInFee);
+            stubCheckInFee.Stub(x => x.GetFee(Arg<Customer>.Is.Anything)).Return(100);
+
+            var customers = new List<Customer>
+            {
+                new Customer { IsMale = true },
+                new Customer { IsMale = false },
+                new Customer { IsMale = false }
+            };
+
+            decimal expected = 1;
+
+            // act
+            var actual = target.CheckIn(customers);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Test_Income()
+        {
+            // arrange
+            ICheckInFee stubCheckInFee = MockRepository.GenerateStub<ICheckInFee>();
+            Pub target = new Pub(stubCheckInFee);
+            stubCheckInFee.Stub(x => x.GetFee(Arg<Customer>.Is.Anything)).Return(100);
+
+            var customers = new List<Customer>
+            {
+                new Customer { IsMale = true },
+                new Customer { IsMale = false },
+                new Customer { IsMale = false }
+            };
+
+            var inComeBeforeCheckIn = target.GetInCome();
+            Assert.AreEqual(0, inComeBeforeCheckIn);
+
+            decimal expectedIncome = 100;
+
+            // act
+            var chargeCustomerCount = target.CheckIn(customers);
+            var actualIncome = target.GetInCome();
+
+            // assert
+            Assert.AreEqual(expectedIncome, actualIncome);
+        }
+    }    
+}
